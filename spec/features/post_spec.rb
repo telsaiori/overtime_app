f619873cb3b52cc9a39b40892201618915bb6150
telsa <telsaiori.git@gmail.com>
2016-12-13 13:42:14 +0800

    add new post button to nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -29,6 +29,14 @@ describe 'navigate' do
     end
   end
 
+  describe "new" do 
+    it "has a link from the homepage" do 
+      visit root_path
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe "creation" do    
     it "has a new form that can be reached" do 
       expect(page.status_code).to eq(200)
