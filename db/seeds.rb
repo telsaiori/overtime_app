351cba52ed29b8bb9d0593556f29e8a8e4d439b0
telsa <telsaiori.git@gmail.com>
2016-12-18 20:12:34 +0800

    add edit status method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 @user = User.create(email: "test@gmail.com", password: '123456', password_confirmation: '123456', first_name: 'aiuchi', last_name: 'haruna')
+
+AdminUser.create(email: "admin@gmail.com", password: '123456', password_confirmation: '123456', first_name: 'admin', last_name: 'name')
+
 100.times do |post|
   @user.posts.create!(date: Date.today, rationale: "#{post} rationale content")
 end
