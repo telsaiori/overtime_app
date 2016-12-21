8c0d64dfb747728d4c7417bb201f9e4dc467efbe
telsa <telsaiori.git@gmail.com>
2016-12-22 00:14:20 +0800

    add audit log dashboard

---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "Auditlog feature" do 
+
+
+  describe "index" do
+    before do 
+      admin_user = FactoryGirl.create(:admin_user)
+      login_as admin_user, scope: :user
+      FactoryGirl.create(:audit_log)
+    end
+    it "has an index page that can be reached" do 
+      visit audit_logs_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it "it renders audit log content" do 
+
+      visit audit_logs_path
+      expect(page).to have_content("RINA, PENG")
+    end
+
+    xit "can not be access by non admin users" do 
+    end
+  end
+end
