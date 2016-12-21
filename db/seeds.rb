fd4923b93562fbcd421a05fd8d2c00b1e8ed166f
telsa <telsaiori.git@gmail.com>
2016-12-21 15:38:47 +0800

    add task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
-@user = User.create(email: "test@gmail.com", password: '123456', password_confirmation: '123456', first_name: 'aiuchi', last_name: 'haruna')
+@user = User.create(email: "test@gmail.com", password: '123456', 
+                    password_confirmation: '123456', 
+                    first_name: 'aiuchi', last_name: 'haruna',
+                    phone: "")
 
-AdminUser.create(email: "admin@gmail.com", password: '123456', password_confirmation: '123456', first_name: 'admin', last_name: 'name')
+AdminUser.create(email: "admin@gmail.com", password: '123456', 
+                 password_confirmation: '123456', 
+                 first_name: 'admin', last_name: 'name',
+                 phone: "")
 
 100.times do |post|
   @user.posts.create!(date: Date.today, rationale: "#{post} rationale content", overtime_request: 2.5)
---
+++ lib/tasks/notification.rake
@@ -0,0 +1,9 @@
+namespace :notification do
+  desc "Sends sms notification to employees asking them to log if they had overtime or not"
+  task sms: :environment do
+    # User.all.each do |user|
+    #   SmsTool.send_sms()
+    # end
+  end
+
+end
