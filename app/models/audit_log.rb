8011a50914ec796d7c43560814edc77b4fdeb8c9
telsa <telsaiori.git@gmail.com>
2016-12-21 19:33:53 +0800

    add validation and defaults to audit log

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,12 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+  validates :user_id, :status, :start_date, presence: true
+
+  after_initialize :set_defaults
+
+  private
+
+    def set_defaults
+      self.start_date ||= Date.today - 6.days
+    end
 end
