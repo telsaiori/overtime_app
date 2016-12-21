2c3dbffd729125a695521d4ae0cad1a7acf963b4
telsa <telsaiori.git@gmail.com>
2016-12-21 18:13:28 +0800

    finished send sms method

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,7 +1,15 @@
 module SmsTool
+  account_sid = ENV['TWILIO_ACCOUNT_SID']
+  auth_token = ENV['TWILIO_AUTH_TOKEN']
 
-  def self.send_sms(number: , message: )
-    puts "sending SMS..."
-    puts "#{message} to #{number}"
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
+  def self.send_sms(number:, message:)
+    @client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: "+886#{number}",
+      body: "#{message}"
+    )
   end
 end
+
