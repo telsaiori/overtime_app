ea6e628585230d6f565a58019a6935611c7ff1d0
telsa <telsaiori.git@gmail.com>
2016-12-22 11:14:01 +0800

    setup employee homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  def admin_types
+    ['AdminUser']
+  end
 
   private
 
