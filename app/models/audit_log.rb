74add14b1c0c28a3bff3c9b09690ca223c1c6b37
telsa <telsaiori.git@gmail.com>
2016-12-26 20:22:44 +0800

    change button css and add scope in audit_log

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -5,6 +5,7 @@ class AuditLog < ApplicationRecord
 
   after_initialize :set_defaults
   before_update :set_end_date, if: :confirmed?
+  scope :by_start_date, -> { order('start_date desc')}
 
   private
 
