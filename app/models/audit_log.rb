fc9dbf145c1094e5cfe49c982748457c07bdc141
telsa <telsaiori.git@gmail.com>
2016-12-26 18:34:32 +0800

    add before_update to set AuditLog's end_date automatically

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -4,8 +4,13 @@ class AuditLog < ApplicationRecord
   validates :user_id, :status, :start_date, presence: true
 
   after_initialize :set_defaults
+  before_update :set_end_date, if: :confirmed?
 
   private
+
+    def set_end_date
+      self.end_date = Date.today
+    end
 
     def set_defaults
       self.start_date ||= Date.today - 6.days
