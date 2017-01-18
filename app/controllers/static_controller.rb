b6d877d6ff9c645c8f88b69df3bc41eb7290578e
telsa <telsaiori.git@gmail.com>
2017-01-18 17:23:41 +0800

    add rake and validates in auditlogs

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,6 +6,9 @@ class StaticController < ApplicationController
       @recent_audit_items = AuditLog.last(10)
     else
       @pending_audit_confirmations = current_user.audit_logs.pending.by_start_date
+      @week_start = Date.today.beginning_of_week
+      @week_end = Date.today.beginning_of_week
+      @no_overtime_week = Post.where(date: @week_start..@week_end)
     end
   end
 end
--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -2,6 +2,7 @@ class AuditLog < ApplicationRecord
   enum status: {pending: 0, confirmed: 1}
   belongs_to :user
   validates :user_id, :status, :start_date, presence: true
+  validates :start_date, uniqueness: { scope: :user_id }
 
   after_initialize :set_defaults
   before_update :set_end_date, if: :confirmed?
